d89474e4823cd36f34659557bf89d8bb843aa702
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-04-17 18:13:31 -0500

    Create the main component

--- src/app/main-page/main-page.module.ts
+++ src/app/main-page/main-page.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TuristicSitesComponent } from './turistic-sites/turistic-sites.component';
 import { TripInfoComponent } from './trip-info/trip-info.component';
+import { MainComponent } from './main/main.component';
 
 
 
 @NgModule({
   declarations: [
     TuristicSitesComponent,
-    TripInfoComponent
+    TripInfoComponent,
+    MainComponent
   ],
   imports: [
     CommonModule
