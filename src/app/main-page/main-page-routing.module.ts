dbef201d2b5b0b70702e6957b5e003d90aeb9910
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-04-18 05:44:29 -0500

    Define the default route in the main-page-routing

--- src/app/main-page/main-page-routing.module.ts
+++ src/app/main-page/main-page-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {MainComponent} from './main/main.component';
 
-const routes: Routes = [{
-  path: '',
-  component: MainComponent
-}];
+const routes: Routes = [
+ { path: '', redirectTo: 'main', pathMatch: 'full'},
+ { path: 'main', component: MainComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
