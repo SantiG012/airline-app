d1b3c4026cdc95c4fc63bd277546d068937cb11a
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-04-18 05:19:33 -0500

    Add the starter rout for the main module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', loadComponent: () => import('./main-page/main-page.module').then(m => m.MainPageModule)}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
