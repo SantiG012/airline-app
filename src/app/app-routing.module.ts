f257f9fee17e01fa6d539918bfaef94bb3ae3711
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-04-18 05:46:15 -0500

    Add the load chlidren to the app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', loadComponent: () => import('./main-page/main-page.module').then(m => m.MainPageModule)}];
+  {path: '', loadChildren: () => import('./main-page/main-page.module').then(m => m.MainPageModule)}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
