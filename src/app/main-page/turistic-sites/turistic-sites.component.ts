a90e960ca19afb03d42573d6e80ccffbb719edb0
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-04-18 11:15:28 -0500

    Add the image change logic

--- src/app/main-page/turistic-sites/turistic-sites.component.ts
+++ src/app/main-page/turistic-sites/turistic-sites.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./turistic-sites.component.css']
 })
 export class TuristicSitesComponent {
+ images: readonly string[] = [
+  "https://images.unsplash.com/photo-1539892613216-9e92b9257fd1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80", 
+  "https://images.unsplash.com/photo-1542704792-e30dac463c90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80", 
+  "https://images.unsplash.com/photo-1508050919630-b135583b29ab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"];
+  count: number = 0;
+  image: string = this.images[0];
 
+  getIindex():number {
+    let index:number = this.count % this.images.length;
+    
+    this.count++;
+    return index;
+  }
+
+  setImage():void {
+    this.image = this.images[this.getIindex()];
+  }
+  
+  ngOnInit() {
+    setInterval(() => {
+      this.setImage();
+    }, 5000);
+  }
+
+  constructor() { }
+  
 }
