634c1c8ae71459ce04a2bf9aa96bdef7bc215a7c
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-04-21 21:28:19 -0500

    Add the forumGroup and the forumControl

--- src/app/main-page/trip-info/trip-info.component.ts
+++ src/app/main-page/trip-info/trip-info.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
-
+import { Component} from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-trip-info',
   templateUrl: './trip-info.component.html',
   styleUrls: ['./trip-info.component.css']
 })
 export class TripInfoComponent {
+  origin: string = "";
+  destination: string = "";
+  passangersNumber: number = 0;
+  cityForm!: FormGroup;
 
+
+  ngOnInit() {
+    this.cityForm = new FormGroup({
+        originControl: new FormControl(
+        this.origin,[
+          Validators.required,
+          Validators.pattern('[a-zA-Z ]+')
+        ]
+      ),
+      destinationControl: new FormControl(
+        this.destination,[
+          Validators.required,
+          Validators.pattern('[a-zA-Z ]+')
+        ]
+      ),
+      passangersNumberControl: new FormControl(
+        this.passangersNumber,[
+          Validators.required,
+          Validators.pattern('^[1-5]$')
+        ]
+      )
+    });
+  }
+
+  get originControl() { return this.cityForm.get('originControl');}
+  get destinationControl() { return this.cityForm.get('destinationControl');}
+  get passangersNumberControl() { return this.cityForm.get('passangersNumberControl');}
 }
