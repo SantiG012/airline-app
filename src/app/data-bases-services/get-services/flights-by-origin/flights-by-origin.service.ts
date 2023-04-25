aa785e12e3571e9e076ba5802e598770755a5053
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-04-25 05:50:30 -0500

    Create the getFlightsByOrigin service incompleted

--- src/app/data-bases-services/get-services/flights-by-origin/flights-by-origin.service.ts
+++ src/app/data-bases-services/get-services/flights-by-origin/flights-by-origin.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@angular/core';
 import { DataBasesServicesModule } from '../../data-bases-services.module';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: DataBasesServicesModule
 })
 export class FlightsByOriginService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  private API_URL = 'http://localhost:8080/vuelo';
+
+  getFlightsByOrigin(origin: string){
+    return this.http.get(`${this.API_URL}/obtenerVuelosPorDestino/${origin}`);
+  }
 }
