39ca72c49ed5b26b204f536104335d661f7fbf7b
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-04-25 05:49:59 -0500

    Add the needed imports

--- src/app/data-bases-services/data-bases-services.module.ts
+++ src/app/data-bases-services/data-bases-services.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlightsByOriginService } from './get-services/flights-by-origin/flights-by-origin.service';
+import{HttpClientModule} from '@angular/common/http';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule
   ],
   providers: [
     FlightsByOriginService
