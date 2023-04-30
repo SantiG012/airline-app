5b03f49994f8c689f93939696a7b75efd8077ecc
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-04-30 08:36:39 -0500

    Create the corroboration function

--- src/app/modules/main-page/services/flight-confirmation.service.ts
+++ src/app/modules/main-page/services/flight-confirmation.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
-
+import { Vuelo } from 'src/app/interfaces/vuelo';
 @Injectable()
 export class FlightConfirmationService {
 
   constructor() { }
 
+  corroborateFlight(origin:string,destiny:string,flights:Vuelo[]): boolean {
+    return flights.some(flight => flight.origen === origin && flight.destino === destiny);
+  }
 }
