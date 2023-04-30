6e198cdb9f5e1266ff68f63ecd3100b94ca3ff12
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-04-30 07:40:41 -0500

    Move the mian-page module to the modules folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', loadChildren: () => import('./main-page/main-page.module').then(m => m.MainPageModule)}];
+  {path: '', loadChildren: () => import('src/app/modules/main-page/main-page.module').then(m => m.MainPageModule)}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
