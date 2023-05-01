953f2d8d119c6211b23bf9d24227e0fbd045b3b1
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-05-01 13:02:47 -0500

    Add the route to the select-flight module

--- src/app/modules/root-module/app-routing.module.ts
+++ src/app/modules/root-module/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
-  {path: '', loadChildren: () => import('src/app/modules/main-page/main-page.module').then(m => m.MainPageModule)}];
+  {path: '', loadChildren: () => import('src/app/modules/main-page/main-page.module').then(m => m.MainPageModule)},
+  {path:'seleccinarVuelos', loadChildren: () => import('src/app/modules/select-flight/select-flight.module').then(m => m.SelectFlightModule),
+    data: {queryParams:{
+      FROM: '',
+      TO: '',
+      SEATS: ''}
+    }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
