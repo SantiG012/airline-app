796990c9ac07fc3b6a20dc3c0374ede516b96463
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-05-01 12:24:32 -0500

    Create the select flight module

---
+++ src/app/modules/select-flight/select-flight.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SelectFlightRoutingModule } from './select-flight-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SelectFlightRoutingModule
+  ]
+})
+export class SelectFlightModule { }
