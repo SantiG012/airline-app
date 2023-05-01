15cde1e5363943ace1cfc90e6d354732db30ecf3
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-05-01 12:32:13 -0500

    Create the flight component

--- src/app/modules/select-flight/select-flight.module.ts
+++ src/app/modules/select-flight/select-flight.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { SelectFlightRoutingModule } from './select-flight-routing.module';
 import { MainComponent } from './components/main/main.component';
 import { BasicInfoComponent } from './components/basic-info/basic-info.component';
+import { FlightComponent } from './components/flight/flight.component';
 
 
 @NgModule({
   declarations: [
     MainComponent,
-    BasicInfoComponent
+    BasicInfoComponent,
+    FlightComponent
   ],
   imports: [
     CommonModule,
