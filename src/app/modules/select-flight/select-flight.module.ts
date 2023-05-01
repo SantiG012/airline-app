ff5f651d8a657ed77734b7e93f1d92752be5a3be
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-05-01 12:30:31 -0500

    Create the main component

--- src/app/modules/select-flight/select-flight.module.ts
+++ src/app/modules/select-flight/select-flight.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SelectFlightRoutingModule } from './select-flight-routing.module';
+import { MainComponent } from './components/main/main.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MainComponent
+  ],
   imports: [
     CommonModule,
     SelectFlightRoutingModule
