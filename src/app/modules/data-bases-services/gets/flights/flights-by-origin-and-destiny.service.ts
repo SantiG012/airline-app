9e0c40b04ca062fef7d8369880f2edc27d4bd9b1
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-05-03 06:12:02 -0500

    Creates the HttpClient and the HttpHeaders objects

--- src/app/modules/data-bases-services/gets/flights/flights-by-origin-and-destiny.service.ts
+++ src/app/modules/data-bases-services/gets/flights/flights-by-origin-and-destiny.service.ts
@@ -1,7 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class FlightsByOriginAndDestinyService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  private API_URL = 'http://localhost:8080/vuelo';
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
 }
