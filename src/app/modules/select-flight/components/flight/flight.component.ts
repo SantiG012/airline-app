23cb114897ed0f88b1d007983b2ad6ecfffcb875
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-05-03 06:23:32 -0500

    Add the input property to the component

--- src/app/modules/select-flight/components/flight/flight.component.ts
+++ src/app/modules/select-flight/components/flight/flight.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Input } from '@angular/core';
+import { Vuelo } from 'src/app/interfaces/vuelo';
 
 @Component({
   selector: 'app-flight',
@@ -6,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./flight.component.css']
 })
 export class FlightComponent {
-
+  @Input() flight!: Vuelo;
 }
