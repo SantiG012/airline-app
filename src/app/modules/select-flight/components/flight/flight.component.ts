5f784dc7174761f772a78f323d94abb205f26e0e
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-05-04 06:48:18 -0500

    Add the formatting methods

--- src/app/modules/select-flight/components/flight/flight.component.ts
+++ src/app/modules/select-flight/components/flight/flight.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Input } from '@angular/core';
 import { Vuelo } from 'src/app/interfaces/vuelo';
+import { DatePipe } from '@angular/common'; 
 
 @Component({
   selector: 'app-flight',
@@ -9,4 +10,14 @@ import { Vuelo } from 'src/app/interfaces/vuelo';
 })
 export class FlightComponent {
   @Input() flight!: Vuelo;
+
+  constructor(private datePipe: DatePipe) { }
+
+  getFormattedHour(date:string): string {
+    return this.datePipe.transform(date, 'HH:mm') || '';
+  }
+
+  getFormattedDate(date:string): string {
+    return this.datePipe.transform(date, 'dd/MM/yyyy') || '';
+  }
 }
