3e359eb7a992cf078856b39a9617e23d89c07c75
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-05-04 07:21:22 -0500

    Make the flights request from the URL params

--- src/app/modules/select-flight/components/main/main.component.ts
+++ src/app/modules/select-flight/components/main/main.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import{FlightsByOriginAndDestinyService} from 'src/app/modules/data-bases-services/gets/flights/flights-by-origin-and-destiny.service';
+import { ActivatedRoute } from '@angular/router';
+import { Vuelo } from 'src/app/interfaces/vuelo';
 
 @Component({
   selector: 'app-main',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent {
+  origin!:string;
+  destiny!:string;
+  flights!:Vuelo[];
+
+  constructor (private flightsByOriginAndDestinyService:FlightsByOriginAndDestinyService,
+               private route: ActivatedRoute){}
+
+  ngOnInit(): void {
+    this.origin = this.route.snapshot.paramMap.get('FROM')!;
+    this.destiny =  this.route.snapshot.paramMap.get('TO')!;
+
+    this.flightsByOriginAndDestinyService.
+    getFlightsByOriginAndDestiny(this.origin,this.destiny)
+    .subscribe(flights => this.flights = flights);
+
+  }
 
 }
