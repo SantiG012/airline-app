65f10a87906e898da06c967d63e2425071a30e25
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-05-05 19:20:58 -0500

    Create the select-seats module

---
+++ src/app/modules/select-seats/select-seats.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SelectSeatsRoutingModule } from './select-seats-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SelectSeatsRoutingModule
+  ]
+})
+export class SelectSeatsModule { }
