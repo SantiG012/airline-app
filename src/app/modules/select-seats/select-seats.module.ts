a04ceff8a487afc8f7cdbaae8f054c6f42bfff8e
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-05-05 19:22:42 -0500

    Generate the main component

--- src/app/modules/select-seats/select-seats.module.ts
+++ src/app/modules/select-seats/select-seats.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SelectSeatsRoutingModule } from './select-seats-routing.module';
+import { MainComponent } from './components/main/main.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MainComponent
+  ],
   imports: [
     CommonModule,
     SelectSeatsRoutingModule
