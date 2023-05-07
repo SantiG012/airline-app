c12e7b698857c326f59ce93777f343d9c47ba5ca
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-05-07 12:48:59 -0500

    Create the DetalleAsiento interface

---
+++ src/app/interfaces/DetalleAsiento.ts
@@ -0,0 +1,10 @@
+export interface DetalleAsiento {
+    idDetalleAsiento: string;
+    precio: number;
+    tipo: string;
+    vueloid: string;
+    columna: number;
+    fila: number;
+    reservaid: string;
+    estado: string;
+}
