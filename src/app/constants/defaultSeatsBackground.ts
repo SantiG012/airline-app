a93024a67ea2a3830ff92e0b43b3138a8f88a6aa
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-05-10 07:00:50 -0500

    Add the defaultSeatsBackground constant

---
+++ src/app/constants/defaultSeatsBackground.ts
@@ -0,0 +1,7 @@
+import { BackgroundTypes } from "../interfaces/backgroundTypes"
+
+export const defaultSeatsBackground: BackgroundTypes = {
+    'economy': 'https://s.latamairlines.com/images/web-ancillaries/seat-map-icons/p-economy-exit.svg',
+    'premium-economy': 'https://s.latamairlines.com/images/web-ancillaries/seat-map-icons/p-economy-standard.svg',
+    'business': 'https://s.latamairlines.com/images/web-ancillaries/seat-map-icons/p-economy-more-space.svg',
+};
