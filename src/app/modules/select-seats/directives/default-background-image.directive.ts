04bde0d4cd44a92b69f241eb58f4198876cce182
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-05-10 07:24:34 -0500

    Modify the defaultBackgroundImage directive logic

--- src/app/modules/select-seats/directives/default-background-image.directive.ts
+++ src/app/modules/select-seats/directives/default-background-image.directive.ts
@@ -1,28 +1,24 @@
 import { Directive, ElementRef, Input } from '@angular/core';
-import { BackgroundTypes } from 'src/app/interfaces/backgroundTypes';
+import {defaultSeatsBackground} from 'src/app/constants/defaultSeatsBackground';
 
 @Directive({
   selector: '[appDefaultBackgroundImage]'
 })
 export class DefaultBackgroundImageDirective {
-  @Input('seatType') seatType!: string;
-  @Input('occupiedSeat') occupiedSeat!: string;
 
   constructor(private elementRef:ElementRef) { }
   readonly DEFAULT_BACKGROUND = 'https://s.latamairlines.com/images/web-ancillaries/seat-map-icons/disabled.svg';
 
-  readonly BACKGROUNDS: BackgroundTypes = {
-    'economy': 'https://s.latamairlines.com/images/web-ancillaries/seat-map-icons/economy.svg',
-    'premium-economy': 'https://s.latamairlines.com/images/web-ancillaries/seat-map-icons/premium-economy.svg',
-    'business': 'https://s.latamairlines.com/images/web-ancillaries/seat-map-icons/business.svg',
-  };
-
   ngOnInit() {
-    if (this.occupiedSeat){
+    const BOOKING = this.elementRef.nativeElement.getAttribute('data-reserva-id');
+    const TYPE = this.elementRef.nativeElement.getAttribute('data-tipo-asiento');
+
+    if (!BOOKING) {
       this.elementRef.nativeElement.style.backgroundImage = `url(${this.DEFAULT_BACKGROUND})`;
       return;
     }
-    const background = this.BACKGROUNDS[this.seatType];
-    this.elementRef.nativeElement.style.backgroundImage = `url(${background})`;
+
+    const BACKGROUND = defaultSeatsBackground[TYPE];
+    this.elementRef.nativeElement.style.backgroundImage = `url(${BACKGROUND})`;
   }
 }
