347324a16032dc09cba8d1f8876e190c66fa98e0
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-05-10 07:42:08 -0500

    Add logic to the hoverBackgroundImage directive

--- src/app/modules/select-seats/directives/hover-background-image.directive.ts
+++ src/app/modules/select-seats/directives/hover-background-image.directive.ts
@@ -1,10 +1,31 @@
-import { Directive } from '@angular/core';
+import { Directive,ElementRef, HostListener} from '@angular/core';
+import {SeatsHoverBackground} from 'src/app/constants/seatsHoverBackground';
+import {defaultSeatsBackground} from 'src/app/constants/defaultSeatsBackground';
 
 @Directive({
   selector: '[appHoverBackgroundImage]'
 })
 export class HoverBackgroundImageDirective {
+  Type!: string;
 
-  constructor() { }
+  constructor(private elementRef:ElementRef) { }
 
+  ngOnInit() {
+    const BOOKING = this.elementRef.nativeElement.getAttribute('data-reserva-id');
+    this.Type = this.elementRef.nativeElement.getAttribute('data-tipo-asiento');
+
+    if (!BOOKING) {
+      return;
+    }
+  }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    const BACKGROUND = SeatsHoverBackground[this.Type];
+    this.elementRef.nativeElement.style.backgroundImage = `url(${BACKGROUND})`;
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    const BACKGROUND = defaultSeatsBackground[this.Type];
+    this.elementRef.nativeElement.style.backgroundImage = `url(${BACKGROUND})`;
+  }
 }
