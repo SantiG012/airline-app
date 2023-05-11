0439e8fe899c4d6e13a1265b565ddeb9619d82b3
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-05-11 06:27:44 -0500

    Call the getSeatsByFlightId in the grid component

--- src/app/modules/select-seats/components/grid/grid.component.ts
+++ src/app/modules/select-seats/components/grid/grid.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DetalleAsiento } from 'src/app/interfaces/DetalleAsiento';
 import {SeatsService} from 'src/app/modules/data-bases-services/gets/seats.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-grid',
@@ -10,5 +11,17 @@ import {SeatsService} from 'src/app/modules/data-bases-services/gets/seats.servi
 export class GridComponent {
   seats!: DetalleAsiento[];
 
-  constructor(private seatsService: SeatsService) {}
+  constructor(private seatsService: SeatsService,
+              private route: ActivatedRoute) {}
+
+  ngOnInit(){
+    const flightId = this.route.snapshot.queryParamMap.get('ID')!;
+
+    this.seatsService.getSeatsByFlightId(flightId).subscribe(
+      (seats: DetalleAsiento[]) => {
+        this.seats = seats;
+      }
+    );
+  }
+
 }
