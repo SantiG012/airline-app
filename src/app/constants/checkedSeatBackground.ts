aee8535c7a3f71675cf1cae09eb82adfd9bed357
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-05-12 17:13:12 -0500

    Add the seatsCheckedBackground constant

---
+++ src/app/constants/checkedSeatBackground.ts
@@ -0,0 +1,7 @@
+import { BackgroundTypes } from "../interfaces/backgroundTypes"
+
+export const seatsCheckedBackground: BackgroundTypes = {
+    'economy': 'https://s.latamairlines.com/images/web-ancillaries/seat-map-icons/p-economy-exit-confirmed.svg',
+    'premium-economy': 'https://s.latamairlines.com/images/web-ancillaries/seat-map-icons/p-economy-standard-confirmed.svg',
+    'business': 'https://s.latamairlines.com/images/web-ancillaries/seat-map-icons/p-economy-more-space-confirmed.svg',
+};
