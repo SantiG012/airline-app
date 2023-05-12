db49cd7df7aca210d616d3610881b5d6a9c39740
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-05-12 17:39:03 -0500

    Add the checked-background-image directive logic

--- src/app/modules/select-seats/directives/checked-background-image.directive.ts
+++ src/app/modules/select-seats/directives/checked-background-image.directive.ts
@@ -1,10 +1,21 @@
-import { Directive } from '@angular/core';
+import { Directive,ElementRef,HostListener} from '@angular/core';
+import { seatsCheckedBackground } from 'src/app/constants/checkedSeatBackground';
 
 @Directive({
   selector: '[appCheckedBackgroundImage]'
 })
 export class CheckedBackgroundImageDirective {
 
-  constructor() { }
+  constructor(private elementRef:ElementRef) { }
+  type!:string;
 
+  ngOnInit() {
+    this.type = this.elementRef.nativeElement.getAttribute('data-tipo-asiento');
+  }
+
+
+  @HostListener('click') onClick() {
+    const BACKGROUND = seatsCheckedBackground[this.type];
+    this.elementRef.nativeElement.style.backgroundImage = `url(${BACKGROUND})`;
+  }
 }
