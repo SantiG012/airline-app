95bfb5478e4110a68d435047b2d9e0f7e58e4bb4
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-05-13 05:58:38 -0500

    Add the SeatStatusService logic

--- src/app/modules/select-seats/services/seat-status.service.ts
+++ src/app/modules/select-seats/services/seat-status.service.ts
@@ -2,6 +2,14 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class SeatStatusService {
-
+  setStatus!: boolean;
   constructor() { }
+
+  getSeatStatus(): boolean {
+    return this.setStatus;
+  }
+
+  setSeatStatus(status: boolean): void {
+    this.setStatus = status;
+  }
 }
