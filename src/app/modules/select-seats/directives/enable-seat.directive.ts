5564aef357318533aad1aba304a6fe02411ccec8
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-05-15 16:14:24 -0500

    Set the default image to unclicked seats

--- src/app/modules/select-seats/directives/enable-seat.directive.ts
+++ src/app/modules/select-seats/directives/enable-seat.directive.ts
@@ -1,6 +1,7 @@
 import { Directive,ElementRef} from '@angular/core';
 import{SeatStatusService} from '../services/seat-status.service';
 import { SeatStatusDTO } from 'src/app/DTOs/SeatStatusDTO';
+import { seatsDefaultImages } from 'src/app/constants/seats-default-images';
 
 @Directive({
   selector: '[appEnableSeat]'
@@ -18,6 +19,16 @@ export class EnableSeatDirective {
   private isSeatAvailable():boolean{
     return this.nonConfirmedSeats.findIndex(seat=>seat.seatId===this.seatId)!==-1;
   }
+
+  private getSeatType():string{
+    return this.elementRef.nativeElement.getAttribute('data-tipo-asiento');
+  }
+
+  private setDefaultSeatImage():void{
+    const DEFAULT_IMAGE=seatsDefaultImages[this.getSeatType()];
+    this.elementRef.nativeElement.style.backgroundImage=`url(${DEFAULT_IMAGE})`;
+  }
+
   
 
   ngAfterViewInit(){
@@ -27,6 +38,7 @@ export class EnableSeatDirective {
         this.nonConfirmedSeats=seats;
         if(this.isSeatAvailable()){
           this.elementRef.nativeElement.style.pointerEvents = 'auto';
+          this.setDefaultSeatImage();
         }
       }
     );
