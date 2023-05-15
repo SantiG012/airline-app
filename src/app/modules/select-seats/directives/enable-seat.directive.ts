9c08a336f53a30dcf8ca16eca74e25e27656244c
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-05-15 08:05:34 -0500

    Add the enable-seat logic

--- src/app/modules/select-seats/directives/enable-seat.directive.ts
+++ src/app/modules/select-seats/directives/enable-seat.directive.ts
@@ -1,10 +1,35 @@
-import { Directive } from '@angular/core';
+import { Directive,ElementRef} from '@angular/core';
+import{SeatStatusService} from '../services/seat-status.service';
+import { SeatStatusDTO } from 'src/app/DTOs/SeatStatusDTO';
+import { Observable,catchError,of, switchMap} from 'rxjs';
 
 @Directive({
   selector: '[appEnableSeat]'
 })
 export class EnableSeatDirective {
+  seatId!:string;
+  nonConfirmedSeats!:SeatStatusDTO[];
+  constructor(private seatStatusService:SeatStatusService,
+              private elementRef:ElementRef) { }
 
-  constructor() { }
+  private getSeatId():string{
+    return this.elementRef.nativeElement.getAttribute('id');
+  }
 
+  private isSeatAvailable():boolean{
+    return this.nonConfirmedSeats.findIndex(seat=>seat.seatId===this.seatId)!==-1;
+  }
+  
+
+  ngAfterViewInit(){
+    this.seatId=this.getSeatId();
+    this.seatStatusService.getNonConfirmedSeatsObservable().subscribe(
+      (seats:SeatStatusDTO[])=>{
+        this.nonConfirmedSeats=seats;
+        if(this.isSeatAvailable()){
+          this.elementRef.nativeElement.style.pointerEvents = 'auto';
+        }
+      }
+    );
+  }
 }
