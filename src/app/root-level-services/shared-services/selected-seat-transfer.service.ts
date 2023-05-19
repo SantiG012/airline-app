6f7a361d9a9dd8c067467929fdf82494d45b77fe
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-05-19 14:43:49 -0500

    Add the transfer service logic

--- src/app/root-level-services/shared-services/selected-seat-transfer.service.ts
+++ src/app/root-level-services/shared-services/selected-seat-transfer.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { SelectedSeatDTO } from 'src/app/DTOs/seatDTOs/selectedSeatDTO';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SelectedSeatTransferService {
-
+  private selectedSeats!: SelectedSeatDTO[];
   constructor() { }
+
+  public setSelectedSeats(selectedSeats: SelectedSeatDTO[]): void {
+    this.selectedSeats = selectedSeats;
+  }
+
+  public getSelectedSeats(): SelectedSeatDTO[] {
+    return this.selectedSeats;
+  }
 }
