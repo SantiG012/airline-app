79ee089909053b145c8a1e7154bd89aedbcb96a0
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-05-19 14:47:57 -0500

    Add the passengers info module

---
+++ src/app/modules/passengers-info/passengers-info.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PassengersInfoRoutingModule } from './passengers-info-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PassengersInfoRoutingModule
+  ]
+})
+export class PassengersInfoModule { }
