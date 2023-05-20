3b2501a812d2fda3d160940b54902346644a62e0
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-05-20 10:50:39 -0500

    Add the passenger forum component

---
+++ src/app/modules/passengers-info/components/passenger-forum/passenger-forum.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-passenger-forum',
+  templateUrl: './passenger-forum.component.html',
+  styleUrls: ['./passenger-forum.component.css']
+})
+export class PassengerForumComponent {
+
+}
--- src/app/modules/passengers-info/passengers-info.module.ts
+++ src/app/modules/passengers-info/passengers-info.module.ts
@@ -4,12 +4,14 @@ import { PriceCalculationService } from './services/price-calculation.service';
 import { PassengersInfoRoutingModule } from './passengers-info-routing.module';
 import { MainComponent } from './components/main/main.component';
 import { BasicInfoComponent } from './components/basic-info/basic-info.component';
+import { PassengerForumComponent } from './components/passenger-forum/passenger-forum.component';
 
 
 @NgModule({
   declarations: [
     MainComponent,
-    BasicInfoComponent
+    BasicInfoComponent,
+    PassengerForumComponent
   ],
   imports: [
     CommonModule,
