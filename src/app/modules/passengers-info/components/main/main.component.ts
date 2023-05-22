f91a95436208351bfe8989d871144e2eb4e146d1
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-05-22 08:54:38 -0500

    Set the forms arrat length

--- src/app/modules/passengers-info/components/main/main.component.ts
+++ src/app/modules/passengers-info/components/main/main.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import { SelectedSeatsTransferService } from 'src/app/root-level-services/shared-services/selected-seats-transfer.service';
+import { ConfirmedSeatDTO } from 'src/app/DTOs/seatDTOs/confirmedSeatDTO';
+import { FormsStateTransferService } from '../../services/forms-state-transfer.service';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
 export class MainComponent {
+  selectedSeats!: ConfirmedSeatDTO[];
+  constructor(private selectedSeatsTransferService: SelectedSeatsTransferService,
+              private formsStateTransferService: FormsStateTransferService) { }
 
+  ngOnInit() {
+    this.selectedSeats = this.selectedSeatsTransferService.getSelectedSeats();
+    this.formsStateTransferService.initializeFormsState(this.selectedSeats.length);
+  }
 }
