f958d6fe9835f05fe141d0eab11618d1481521b3
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-05-24 16:42:49 -0500

    Add the date formatting logic

--- src/app/modules/search-flights/components/flight/flight.component.ts
+++ src/app/modules/search-flights/components/flight/flight.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common'; 
+import { Router } from '@angular/router';
+import { Vuelo } from 'src/app/interfaces/vuelo';
+import {FlightsService} from 'src/app/modules/data-bases-services/gets/flights.service';
 
 @Component({
   selector: 'app-flight',
@@ -6,5 +10,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./flight.component.css']
 })
 export class FlightComponent {
+  constructor (private datePipe: DatePipe,
+              private router:Router,
+              private flightsService:FlightsService
+    ) {}
 
+  getFormattedHour(date:string): string {
+    return this.datePipe.transform(date, 'HH:mm') || '';
+  }
+
+  getFormattedDate(date:string): string {
+    return this.datePipe.transform(date, 'dd/MM/yyyy') || '';
+  }
+
+  onClick() {
+    //this.router.navigate(['seleccionarAsientos'], { queryParams: { ID:this.flight.id,SEATS:this.SEATS} });
+  }
 }
