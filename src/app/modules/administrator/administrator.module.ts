9b7c94a86a67079a9fe117a72aadc4fb8a06f3cc
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-05-26 09:17:20 -0500

    Add the needed angular material modules

--- src/app/modules/administrator/administrator.module.ts
+++ src/app/modules/administrator/administrator.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { AdministratorRoutingModule } from './administrator-routing.module';
 import { LoginComponent } from './components/login/login.component';
 
@@ -11,7 +12,9 @@ import { LoginComponent } from './components/login/login.component';
   ],
   imports: [
     CommonModule,
-    AdministratorRoutingModule
+    AdministratorRoutingModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class AdministratorModule { }
