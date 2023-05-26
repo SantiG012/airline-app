f2f2bc14aa2b33cd9cfc964ea40933e3de99ef5a
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-05-26 07:30:49 -0500

    Add the get bookingId logic

--- src/app/modules/search-flights/services/payment-corroboration.service.ts
+++ src/app/modules/search-flights/services/payment-corroboration.service.ts
@@ -4,12 +4,22 @@ import { Booking } from 'src/app/interfaces/booking';
 @Injectable()
 export class PaymentCorroborationService {
 
+  flightIdAndBookingId: { [flightId: string]:string } = {};
+
   constructor() { }
 
   determineFlightPaymentStatus(flightId: string, bookings: Booking[]): boolean {
     const flightBooking = bookings.filter(booking => booking.vueloId === flightId);
     const flightPaymentStatus = flightBooking[0].estadoPago;
+
+    this.flightIdAndBookingId[flightId] = flightBooking[0].reservaId;
+    
     if (flightPaymentStatus === "f")return false;
     return true;
   }
+
+  getBookingId(flightId: string): string {
+    return this.flightIdAndBookingId[flightId];
+  }
+
 }
