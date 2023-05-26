faa9a9ef02a34088e66a930bc950165af6ad151a
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-05-26 09:45:16 -0500

    Create and style the login page

--- src/app/modules/administrator/administrator.module.ts
+++ src/app/modules/administrator/administrator.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatIconModule} from '@angular/material/icon';
 import { AdministratorRoutingModule } from './administrator-routing.module';
 import { LoginComponent } from './components/login/login.component';
 
@@ -14,7 +15,8 @@ import { LoginComponent } from './components/login/login.component';
     CommonModule,
     AdministratorRoutingModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatIconModule
   ]
 })
 export class AdministratorModule { }
