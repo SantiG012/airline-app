74624f691ed7ca7703cc6b6d114cacc43ef39927
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-05-27 10:54:58 -0500

    Replace the canActivateChild guard

--- src/app/root-level-guards/admin-authentication.guard.ts
+++ src/app/root-level-guards/admin-authentication.guard.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivateChild, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthorizationService } from '../root-level-services/authorization.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AdminAuthenticationGuard implements CanActivateChild {
+export class AdminAuthenticationGuard  {
 
   constructor(
     private authorizationService: AuthorizationService
   ) { }
 
-  canActivateChild(
+  canActivate(
     childRoute: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return this.authorizationService.isLogIn();
