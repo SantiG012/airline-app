731f061e46388da995c4b214cee45d41eb7f20bd
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-05-27 11:11:10 -0500

    Add the redirect logic

--- src/app/root-level-guards/admin-authentication.guard.ts
+++ src/app/root-level-guards/admin-authentication.guard.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Injectable,} from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthorizationService } from '../root-level-services/authorization.service';
 
@@ -9,12 +9,16 @@ import { AuthorizationService } from '../root-level-services/authorization.servi
 export class AdminAuthenticationGuard  {
 
   constructor(
-    private authorizationService: AuthorizationService
+    private authorizationService: AuthorizationService,
+    private router: Router
   ) { }
 
   canActivate(
     childRoute: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    if (!this.authorizationService.isLogIn()) this.router.navigate(['administrador/login']);
+
     return this.authorizationService.isLogIn();
   }
   
