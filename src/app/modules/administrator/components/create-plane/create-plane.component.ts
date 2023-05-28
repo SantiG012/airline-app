85d5cc096871393ffb2ad286eb9f2db4bbf1efac
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-05-28 14:31:38 -0500

    Create the formGroup in the component

--- src/app/modules/administrator/components/create-plane/create-plane.component.ts
+++ src/app/modules/administrator/components/create-plane/create-plane.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PlanePostsService } from 'src/app/modules/data-bases-services/posts/plane-posts.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-plane',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-plane.component.css']
 })
 export class CreatePlaneComponent {
+  planeForm!: FormGroup;
 
+  constructor(
+    private planePostsService: PlanePostsService
+  ) { }
+
+  ngOnInit(){
+    this.planeForm = new FormGroup({
+      planeTypeControl: new FormControl(
+        null,[
+          Validators.required,
+          Validators.pattern('^[A-zÀ-ú ]+$')
+        ]
+      ),
+    });
+    
+  }
+
+  onButtonClicked(){
+  }
+
+  get planeTypeControl() { return this.planeForm.get('planeTypeControl'); }
 }
