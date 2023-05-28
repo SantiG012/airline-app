6a2334eb0c10267f18efb6d93a457e0180346e19
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-05-28 09:27:15 -0500

    Create and style the create plane component

---
+++ src/app/modules/administrator/components/create-plane/create-plane.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-plane',
+  templateUrl: './create-plane.component.html',
+  styleUrls: ['./create-plane.component.css']
+})
+export class CreatePlaneComponent {
+
+}
