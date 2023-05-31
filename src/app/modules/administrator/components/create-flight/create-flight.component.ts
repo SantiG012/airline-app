c6d98219fcee03e7e540120014395608bece2ec8
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-05-31 17:13:54 -0500

    Add the form controls

--- src/app/modules/administrator/components/create-flight/create-flight.component.ts
+++ src/app/modules/administrator/components/create-flight/create-flight.component.ts
@@ -1,15 +1,78 @@
 import { Component } from '@angular/core';
-import { MatDatepicker} from '@angular/material/datepicker';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-flight',
   templateUrl: './create-flight.component.html',
-  styleUrls: ['./create-flight.component.css'],
-  providers: [
-    MatDatepicker,
-  ]
+  styleUrls: ['./create-flight.component.css']
 })
-export class CreateFlightComponent {
+export class CreateFlightComponent {  
+  flightForm!: FormGroup;
+
+  constructor() { }
+
+  ngOnInit(){
+    this.flightForm = new FormGroup({
+      departureCityControl: new FormControl(
+        null,[
+          Validators.required,
+          Validators.pattern('^[A-zÀ-ú ]+$')
+        ]
+      ),
+      arrivalCityControl: new FormControl(
+        null,[
+          Validators.required,
+          Validators.pattern('^[A-zÀ-ú ]+$')
+        ]
+      ),
+      departureDateControl: new FormControl(
+        null,[
+          Validators.required
+        ]
+      ),
+      arrivalDateControl: new FormControl(
+        null,[
+          Validators.required
+        ]
+      ),
+      departureHoursControl: new FormControl(
+        null,[
+          Validators.required,
+          Validators.pattern('^00$|^[0-9]{2}$'),
+          Validators.min(0),
+          Validators.max(23)
+        ]
+      ),
+      departureMinutesControl: new FormControl(
+        null,[
+          Validators.required,
+          Validators.pattern('^[0-9]{2}$'),
+          Validators.min(0),
+          Validators.max(59)
+        ]
+      ),
+      arrivalHoursControl: new FormControl(
+        null,[
+          Validators.required,
+          Validators.pattern('^[0-9]{2}$'),
+          Validators.min(0),
+          Validators.max(23)
+        ]
+      ),
+      arrivalMinutesControl: new FormControl(
+        null,[
+          Validators.required,
+          Validators.pattern('^[0-9]{2}$'),
+          Validators.min(0),
+          Validators.max(59)
+        ]
+      ),
+      planeIdControl: new FormControl(
+        null,[
+          Validators.required
+        ]
+      )
+    });
+  }
 
 }
