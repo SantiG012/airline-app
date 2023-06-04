fc3321070d73f29a0bb57e7e277a9756369d4633
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-06-04 08:28:11 -0500

    Add the request logic

--- src/app/modules/administrator/components/dashboards-components/modify-flight-components/modify-cities/modify-cities.component.ts
+++ src/app/modules/administrator/components/dashboards-components/modify-flight-components/modify-cities/modify-cities.component.ts
@@ -1,6 +1,8 @@
 import { Component,Input} from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { IVuelo } from 'src/app/interfaces/IVuelo';
+import { FlightPutsService } from 'src/app/modules/data-bases-services/puts/flight-puts.service';
+import {HttpErrorResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-modify-cities',
@@ -10,8 +12,11 @@ import { IVuelo } from 'src/app/interfaces/IVuelo';
 export class ModifyCitiesComponent {
   CitiesForm!:FormGroup;
   @Input() flightInput!:IVuelo;
+  isFlightModified!:boolean;
 
-  constructor() { }
+  constructor(
+    private flightPutsService: FlightPutsService
+  ) { }
 
   ngOnInit(){
     this.CitiesForm = new FormGroup({
@@ -29,6 +34,50 @@ export class ModifyCitiesComponent {
       )
     });
   }
+
+  private areFormsValid():boolean{
+    if(this.CitiesForm.invalid){
+      alert('Por favor, complete el formulario de ciudades adecuadamente'); 
+      return false;
+    }
+
+    if(this.CitiesForm.value.departureCityControl === this.CitiesForm.value.arrivalCityControl){
+      alert('La ciudad de origen y la ciudad de destino no pueden ser iguales');
+      return false;
+    }
+
+    return true;
+  }
+
+  onSaveButtonClicked(){
+
+    if(!this.areFormsValid())return;
+
+    this.flightInput.origen = this.CitiesForm.value.departureCityControl;
+    this.flightInput.destino = this.CitiesForm.value.arrivalCityControl;
+
+    this.flightPutsService.modifyDepartureAndArrivalCities(this.flightInput).subscribe({
+      error: (error: HttpErrorResponse) => {
+        if(error.status === 0){
+          alert("Intente más tarde. No hay conexión")
+          return;
+        }
+
+        alert("Error: "+error.message)
+      },
+      complete: () => {
+        this.isFlightModified = true;
+
+        setTimeout(() => {
+          this.isFlightModified = false;
+        }
+        , 4000);
+      }
+
+    });
+
+  }
+
 
   get departureCityControl() { return this.CitiesForm.get('departureCityControl'); }
   get arrivalCityControl() { return this.CitiesForm.get('arrivalCityControl'); }
