3f56086d4337b4b8798a224625dc2b198cacd7ab
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-06-06 08:06:06 -0500

    Create and style the fetch seats component

--- src/app/modules/administrator/components/modify-seats-price-components/modify-seats-price/modify-seats-price.component.ts
+++ src/app/modules/administrator/components/modify-seats-price-components/modify-seats-price/modify-seats-price.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { SeatsService } from 'src/app/modules/data-bases-services/gets/seats.service';
 
 @Component({
   selector: 'app-modify-seats-price',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./modify-seats-price.component.css']
 })
 export class ModifySeatsPriceComponent {
+  fetchFlightForm!:FormGroup;
+  areSeatsFetched:boolean = false;
+
+  constructor(
+    private seatsService: SeatsService
+  ) { }
+
+  ngOnInit(){
+    this.createForm();
+  }
+
+  private createForm(){
+    this.fetchFlightForm = new FormGroup({
+      flightIdControl: new FormControl(
+        null,[
+          Validators.required
+        ]
+      )
+    });
+  }
+
+  get flightIdControl() { return this.fetchFlightForm.get('flightIdControl'); }
 
 }
