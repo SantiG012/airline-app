db77a795d2c584ad7d9243d7ebb4d5e328474d87
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-06-06 18:26:56 -0500

    Add the form group

--- src/app/modules/administrator/components/modify-seats-price-components/modify-seat-form/modify-seat-form.component.ts
+++ src/app/modules/administrator/components/modify-seats-price-components/modify-seat-form/modify-seat-form.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FlightIdTransferService } from '../../../services/flight-id-transfer.service';
 
 @Component({
   selector: 'app-modify-seat-form',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./modify-seat-form.component.css']
 })
 export class ModifySeatFormComponent {
+  seatForm!:FormGroup;
+  
+  constructor(
+    private flightIdTransferService:FlightIdTransferService
+  ) { }
 
+  ngOnInit(){
+    this.createForm();
+  }
+
+  private createForm(){
+    this.seatForm = new FormGroup({
+      seatPriceControl: new FormControl(
+        null,[
+          Validators.required,
+          Validators.pattern('^[0-9,]*$'),
+          Validators.min(25000),
+          Validators.max(100000)
+        ]
+      )
+    });
+  }
+
+  get seatPriceControl() { return this.seatForm.get('seatPriceControl'); }
 }
