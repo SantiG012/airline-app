11a38f70624304da786bce5edbd2ea49ef558f3d
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-06-06 18:45:37 -0500

    Remove unused code

--- src/app/modules/administrator/components/modify-seats-price-components/modify-seat-form/modify-seat-form.component.ts
+++ src/app/modules/administrator/components/modify-seats-price-components/modify-seat-form/modify-seat-form.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { FlightIdTransferService } from '../../../services/flight-id-transfer.service';
+import { DetalleAsiento } from 'src/app/interfaces/DetalleAsiento';
 
 @Component({
   selector: 'app-modify-seat-form',
@@ -8,28 +8,10 @@ import { FlightIdTransferService } from '../../../services/flight-id-transfer.se
   styleUrls: ['./modify-seat-form.component.css']
 })
 export class ModifySeatFormComponent {
-  seatForm!:FormGroup;
+
   
   constructor(
     private flightIdTransferService:FlightIdTransferService
   ) { }
 
-  ngOnInit(){
-    this.createForm();
-  }
-
-  private createForm(){
-    this.seatForm = new FormGroup({
-      seatPriceControl: new FormControl(
-        null,[
-          Validators.required,
-          Validators.pattern('^[0-9]*$'),
-          Validators.min(25000),
-          Validators.max(100000)
-        ]
-      )
-    });
-  }
-
-  get seatPriceControl() { return this.seatForm.get('seatPriceControl'); }
 }
