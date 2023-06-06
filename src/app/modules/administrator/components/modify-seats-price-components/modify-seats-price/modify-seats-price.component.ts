9f19b21923aa74089faf60762ff29d600d6a7de7
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-06-06 08:36:16 -0500

    Add the seats request

--- src/app/modules/administrator/components/modify-seats-price-components/modify-seats-price/modify-seats-price.component.ts
+++ src/app/modules/administrator/components/modify-seats-price-components/modify-seats-price/modify-seats-price.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { HttpErrorResponse } from '@angular/common/http';
 import { SeatsService } from 'src/app/modules/data-bases-services/gets/seats.service';
+import { DetalleAsiento } from 'src/app/interfaces/DetalleAsiento';
 
 @Component({
   selector: 'app-modify-seats-price',
@@ -28,6 +30,27 @@ export class ModifySeatsPriceComponent {
       )
     });
   }
+
+  onFetchSeatsClick(){
+    this.seatsService.getSeatsByFlightId(this.flightIdControl!.value.trim()).subscribe({
+      error: (error:HttpErrorResponse) => {
+        if(error.status === 0){
+          alert('Error de conexión con el servidor. Intente más tarde.');
+        }
+
+        alert('Error al obtener los asientos: '+error.message);
+      },
+      complete: () => {
+        this.areSeatsFetched = true;
+
+        setTimeout(() => {
+          this.areSeatsFetched = false;
+        }
+        , 4000);
+      }
+    });
+  }
+
 
   get flightIdControl() { return this.fetchFlightForm.get('flightIdControl'); }
 
