810735c4cf2f8aea5b8071d6ef12f0e19eaf8027
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-06-07 12:13:06 -0500

    Add exception handling

--- src/app/modules/data-bases-services/gets/invoice-get.service.ts
+++ src/app/modules/data-bases-services/gets/invoice-get.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient,HttpHeaders } from '@angular/common/http';
-import { Observable,catchError,tap,of} from 'rxjs';
+import { HttpClient,HttpErrorResponse,HttpHeaders } from '@angular/common/http';
+import { Observable,catchError,tap, throwError} from 'rxjs';
 import { Invoice } from 'src/app/interfaces/invoice';
 
 @Injectable()
@@ -19,15 +19,15 @@ export class InvoiceGetService {
     return this.http.get<Invoice>(`${this.API_URL}/obtenerFactura/${bookingId}`)
     .pipe(
       tap(_ => console.log(`fetched invoice by bookingId=${bookingId}`)),
-      catchError(this.handleError<Invoice>(`getInvoiceByBookingId bookingId=${bookingId}`))
+      catchError(this.handleError(`getInvoiceByBookingId bookingId=${bookingId}`))
     );
   }
 
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => { 
+  private handleError(operation = 'fetch invoice') {
+    return (error: HttpErrorResponse): Observable<never> => { 
       console.error(error); 
       console.log(`${operation} failed: ${error.message}`);
-      return of(result as T);
+      return throwError(()=>error)
     };
   }
 }
--- src/app/modules/data-bases-services/puts/booking-put.service.ts
+++ src/app/modules/data-bases-services/puts/booking-put.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient,HttpHeaders} from '@angular/common/http';
-import { Observable,of,catchError,tap} from 'rxjs';
+import { HttpClient,HttpErrorResponse,HttpHeaders} from '@angular/common/http';
+import { Observable,of,catchError,tap, throwError} from 'rxjs';
 import { Booking } from 'src/app/interfaces/booking';
 
 @Injectable()
@@ -16,18 +16,18 @@ export class BookingPutService {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
 
-  payBooking(bookingId:string):Observable<Booking>{
-    return this.http.put<Booking>(`${this.API_URL}/pagarReserva/${bookingId}`, this.httpOptions).pipe(
-        tap((newBooking: Booking) => console.log(`paid booking w/ id=${newBooking.reservaId}`)),
-        catchError(this.handleError<Booking>('payBooking'))
+  payBooking(bookingId:string):Observable<any>{
+    return this.http.put<any>(`${this.API_URL}/pagarReserva/${bookingId}`, this.httpOptions).pipe(
+        tap((_) => console.log(`paid booking`)),
+        catchError(this.handleError('payBooking'))
       );
   }
   
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => { 
+  private handleError(operation = 'fetch invoice') {
+    return (error: HttpErrorResponse): Observable<never> => { 
       console.error(error); 
       console.log(`${operation} failed: ${error.message}`);
-      return of(result as T);
+      return throwError(()=>error)
     };
   }
 }
