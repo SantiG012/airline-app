842f599e2cd119cddea37e01a7d50e4adc59a341
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-06-07 08:04:19 -0500

    Bind the events to the component

--- src/app/modules/administrator/components/dashboards-components/airplane-components/search-planes/search-planes.component.ts
+++ src/app/modules/administrator/components/dashboards-components/airplane-components/search-planes/search-planes.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { IPlane } from 'src/app/interfaces/IPlane';
+import { PlaneGetsService } from 'src/app/modules/data-bases-services/gets/plane-gets.service';
+import { Observable, tap } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Clipboard } from '@angular/cdk/clipboard';
 
 @Component({
   selector: 'app-search-planes',
@@ -6,5 +11,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./search-planes.component.css']
 })
 export class SearchPlanesComponent {
+  planes:IPlane[] = []; 
+
+  constructor(
+    private planeGetsService: PlaneGetsService,
+    private clipboard: Clipboard
+  ) { }
+
+  onCopyIdClick(id: string) {
+    this.clipboard.copy(id);
+  }
+
+  onSearchPlanesButtonClick() {
+    this.planeGetsService.getPlanes().subscribe({
+      next: (planes:IPlane[]) => {
+        this.planes = planes;
+      },
+      error: (err: HttpErrorResponse) => {
+        if (err.status === 0) {
+          console.log('No hay conexion con el servidor');
+        }
+
+        alert('Ha ocurrido un error al obtener los aviones: '+ err.message);
+      }
+    })
+
+  }
+
+
 
 }
