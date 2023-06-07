ef7b3d9dcf1b50eedbe8a0a6a50e08a31ee603ae
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-06-07 08:22:05 -0500

    Add exception handling

--- src/app/modules/data-bases-services/posts/user-post.service.ts
+++ src/app/modules/data-bases-services/posts/user-post.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient,HttpHeaders} from '@angular/common/http';
-import { Observable,of,catchError,tap} from 'rxjs';
+import { HttpClient,HttpHeaders,HttpErrorResponse} from '@angular/common/http';
+import { Observable,catchError,tap, throwError} from 'rxjs';
 import { User } from 'src/app/interfaces/user';
 
 
@@ -20,15 +20,15 @@ export class UserPostService {
   postUser(user: User):Observable<User>{
     return this.http.post<User>(`${this.API_URL}/guardarUsuario`, user).pipe(
        tap((newUser: User) => console.log(`added user w/ id=${newUser.usuarioId}`)),
-       catchError(this.handleError<User>('postUser'))
+       catchError(this.handleError('postUser'))
       );
   }
 
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => { 
+  private handleError(operation = 'operation') {
+    return (error: HttpErrorResponse): Observable<never> => { 
       console.error(error); 
       console.log(`${operation} failed: ${error.message}`);
-      return of(result as T);
+      return throwError(()=> error);
     };
   }
 }
